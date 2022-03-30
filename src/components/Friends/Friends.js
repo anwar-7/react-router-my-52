311f4ef37c40180cdff1b6623c27b59bf6d9dc2a
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-03-31 03:56:10 +0600

    update router with components

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((json) => setFriends(json));
+  }, []);
   return (
     <div>
       <h2>Hello Friends.. How are you..</h2>
       <p>
-        <small>parle kicu taka dar dis...</small>
+        <small>Parle kicu taka dar dis..: {friends.length} </small>
       </p>
+
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
     </div>
   );
 };
