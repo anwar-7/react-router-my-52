26af80e66ff3714b315be7463c4461b321cce933
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-03-30 20:03:39 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>This is about us</h2>
+      <p>amader sompoke jante caow</p>
+      <p>
+        <small>Tile age birani khaow...</small>
+      </p>
+    </div>
+  );
+};
+
+export default About;
