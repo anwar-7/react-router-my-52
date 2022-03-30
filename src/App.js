0ddfdf6125be7b7362bbb2e56744a47dcdae6c10
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-03-31 04:43:43 +0600

    reright some code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './components/About/About';
-import Friends from './components/Friends/Friends';
-import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import About from './components/About/About';
+import Header from './components/Header/Header';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
